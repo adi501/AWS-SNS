df73649865602d3a6aec29932e6057189881b0d5
Adinarayana Reddy JC <jc.adi101@gmail.com>
2024-10-29 21:28:25 +0530

    Add project files.

---
+++ Example.PublisherApplication/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Amazon.SimpleNotificationService;
+using Amazon.Runtime;
+using Amazon;
+
+namespace Example.PublisherApplication
+{
+    class Program
+    {
+        static void Main()
+        {
+            Run().GetAwaiter().GetResult();
+        }
+
+        private static async Task Run()
+        {
+            var credentials = new BasicAWSCredentials("accessKey", "secretKey");
+
+            using (var snsClient = new AmazonSimpleNotificationServiceClient(credentials, RegionEndpoint.USEast1)) //RegionEndpoint.USEast1 is a region name
+            {
+                var publisher = new Publisher(snsClient, "topicName");
+
+                while (true)
+                {
+                    Console.WriteLine("Type a message to send:");
+                    var message = Console.ReadLine();
+
+                    await publisher.PublishAsync(message);
+                }
+            }
+        }
+    }
+}
---
+++ Example.SubscriberApplication/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.Auth.AccessControlPolicy;
+using Amazon.Runtime;
+using Amazon.SimpleNotificationService;
+using Amazon.SQS;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Example.SubscriberApplication
+{
+    class Program
+    {
+        static void Main()
+        {
+            Run().GetAwaiter().GetResult();
+        }
+
+        private static async Task Run()
+        {
+            var credentials = new BasicAWSCredentials("accessKey", "secretKey");
+
+            using (var sqsClient = new AmazonSQSClient(credentials, RegionEndpoint.USEast1)) //RegionEndpoint.USEast1 is a region name
+            using (var snsClient = new AmazonSimpleNotificationServiceClient(credentials,RegionEndpoint.USEast1))
+            {
+                var subscriber = new Subscriber(sqsClient, snsClient, "topicName", "queueName");
+
+                Console.WriteLine("Listening...");
+
+                await subscriber.ListenAsync(message => {
+
+                    if (message != null)
+                    {
+                        dynamic data = JObject.Parse(message.Body);
+                       // Console.WriteLine(data.Message);
+                        Console.WriteLine("Received message(only message): " + data.Message);
+                        Console.WriteLine("Received message (Total Info): " + message.Body);
+                    }
+                });
+            }
+        }
+    }
+}
